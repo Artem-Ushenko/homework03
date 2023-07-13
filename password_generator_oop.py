971723c45ddaf95cbbb415bdd0a343f25911576b
Artem Ushenko <kingart030192kingart@gmail.com>
2023-07-13 14:54:52 +0300

    Was added default conditions and tested the program on them

--- password_generator_oop.py
+++ password_generator_oop.py
@@ -5,21 +5,28 @@ import string
 class PasswordGenerator:
     """Was created class for object Password Generator"""
 
-    def __init__(self, length): #, include_uppercase, include_lowercase, include_digits, include_special_chars):
+    def __init__(self, length, include_uppercase, include_lowercase, include_digits, include_special_chars):
         '''Method init with some conditions for object Password Generator'''
         self.length = length #default: 8
-        # self.include_uppercase = include_uppercase #default: True
-        # self.include_lowercase = include_lowercase #default: True
-        # self.include_digits = include_digits #default: True
-        # self.include_special_chars = include_special_chars #default: True
+        self.include_uppercase = include_uppercase #default: True
+        self.include_lowercase = include_lowercase #default: True
+        self.include_digits = include_digits #default: True
+        self.include_special_chars = include_special_chars #default: True
 
     def generator(self):
+
         uppercase_letters = string.ascii_uppercase
         lowercase_letters = string.ascii_lowercase
-        for item in range(self.length):
-            print(random.choice(uppercase_letters + lowercase_letters))
+        symbols = string.punctuation
+        digits = string.digits
+        your_password = ''
+
+        if self.include_uppercase is True and self.include_lowercase is True and self.include_digits is True and self.include_special_chars is True:
+            for item in range(self.length):
+                your_password += random.choice(uppercase_letters + lowercase_letters + symbols + digits)
+
+        print(your_password)
 
 
-
-your_password = PasswordGenerator(int(input("Enter length: ")))
+your_password = PasswordGenerator(8, True, True, True, True)
 your_password.generator()
