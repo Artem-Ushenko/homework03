08e1743da40f0395fd353ea809dd1905173c8439
Artem Ushenko <kingart030192kingart@gmail.com>
2023-07-13 13:51:05 +0300

    Was created class PasswordGenerator and generator function

---
+++ password_generator_oop.py
@@ -0,0 +1,25 @@
+import random
+import string
+
+
+class PasswordGenerator:
+    """Was created class for object Password Generator"""
+
+    def __init__(self, length): #, include_uppercase, include_lowercase, include_digits, include_special_chars):
+        '''Method init with some conditions for object Password Generator'''
+        self.length = length #default: 8
+        # self.include_uppercase = include_uppercase #default: True
+        # self.include_lowercase = include_lowercase #default: True
+        # self.include_digits = include_digits #default: True
+        # self.include_special_chars = include_special_chars #default: True
+
+    def generator(self):
+        uppercase_letters = string.ascii_uppercase
+        lowercase_letters = string.ascii_lowercase
+        for item in range(self.length):
+            print(random.choice(uppercase_letters + lowercase_letters))
+
+
+
+your_password = PasswordGenerator(int(input("Enter length: ")))
+your_password.generator()
