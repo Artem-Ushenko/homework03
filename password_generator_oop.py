0746b5c62040cb0c06c8e2514d2aeba920f7b138
Artem Ushenko <kingart030192kingart@gmail.com>
2023-07-18 13:59:45 +0300

    Was backed to previous commit cda82b6ed0ab629354db8754f3038ac2bdf3995a

--- password_generator_oop.py
+++ password_generator_oop.py
@@ -133,32 +133,3 @@ your_password = PasswordGenerator(length, include_uppercase, include_lowercase, 
 
 # Print the value to the CLI
 print(f"Your password: {your_password.generator()}")
-
-
-
-def get_input_parameter(prompt):
-    """Check input parameter which must be only yes or no"""
-    response = input(prompt)
-    if response.lower() in ['yes', 'no']:
-        return response.lower() == 'yes'
-    else:
-        print("Please enter 'yes' or 'no' !!!")
-        return get_input_parameter(prompt)
-
-
-# Get the password length from the user, using a default value if no input is given
-while True:
-    try:
-        length_input = input("Enter the desired password length (default: 8): ")
-        length = int(length_input) if length_input else 8
-        break
-    except ValueError:
-        print("Invalid input. Please enter an integer number.")
-
-include_uppercase = get_input_parameter("Include uppercase letters (yes/no): ")
-include_lowercase = get_input_parameter("Include lowercase letters (yes/no): ")
-include_digits = get_input_parameter("Include digits (yes/no): ")
-include_special_chars = get_input_parameter("Include special symbols (yes/no): ")
-
-your_password = PasswordGenerator(length, include_uppercase, include_lowercase, include_digits, include_special_chars)
-print(f"Your password: {your_password.generator()}")
